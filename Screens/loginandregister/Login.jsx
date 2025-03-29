8f8388dc23bd90657acb99586ad92391219830d1
Muskaan Cheema <muskaancheema3027@gmail.com>
2025-03-29 23:03:54 +0530

    change the loginpage

--- Screens/loginandregister/Login.jsx
+++ Screens/loginandregister/Login.jsx
@@ -1,4 +1,4 @@
-const {View,Text,Image, TextInput} = require('react-native');
+const {View,Text,Image, TextInput, TouchableOpacity} = require('react-native');
 import styles from './Style';
 import Feather from 'react-native-vector-icons/Feather';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
@@ -16,8 +16,23 @@ function LoginPage(){
                 <FontAwesome name="user-o" color="#420475" style={styles.smallIcon} />
                 <TextInput placeholder='EmailId' style={styles.textInput} />
             </View>
+            <View styles={styles.loginContainer}>
+                <Text style={styles.text_header}>Login!!!</Text>
+                <FontAwesome name="lock" color="#420475" style={styles.smallIcon} />
+                <TextInput placeholder='Password' style={styles.textInput} />
+            </View>
+            <View style={styles.button}>
+                <TouchableOpacity style={styles.inBut}>
+                    <View>
+                        <Text style={styles.textSign}>Login</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 
 export default LoginPage;
+
+
+
